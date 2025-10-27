e0d201a21d788c11febb6c40625f8362df693ebb
muhamadfahrilakbar8-cyber <muhamadfahrilakbar8@gmail.com>
2025-10-28 04:52:45 +0700

    Add files via upload

---
+++ latihan 2.py
@@ -0,0 +1,7 @@
+a =int(input("masukan bilangan pertama:" ))
+b =int(input("masukan bilangan kedua:" ))
+c =int(input("masukan bilangan ketiga:" ))
+
+masukan= [a,b,c]
+masukan.sort()
+print("urutan bilangan :",masukan)
